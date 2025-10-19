5864476793122f7595bb8c179a24ec2305415852
natcore-tech <asesoriaelectr@gmail.com>
2025-10-19 15:19:19 -0500

    Seguimiento repositorio semana3

---
+++ modulo2/javascript_mp/03_operadores_aritmeticos_mp.js
@@ -0,0 +1,49 @@
+console.log("OPERADORES ARITMETICOS NOCTURNA");
+
+numero1=120000;
+numero2=240000;
+console.log("Suma de unidades vendidas del AE86:");
+suma = numero1 + numero2;
+console.log("Total",suma);
+
+console.log("Resta de precio entre AE86 y GR86:");
+resta = numero1 - numero2;
+console.log("Resultado",resta);
+
+console.log("AE86 (precio) por unidad vendida:");
+multiplicacion = numero1 * numero2;
+console.log("Resultado",multiplicacion);
+
+console.log("Unidades vendidas entre sucursales:");
+division = numero1 / numero2;
+console.log("Resultado",division);
+
+console.log("Modelos 86 no vendidos:");
+modulo = numero1 % numero2;
+console.log("Resultado",modulo);
+
+console.log("Modelo 86 de mayor valor:");
+potencia = numero1 ** numero2;
+console.log("Resultado",potencia);
+
+
+console.log("OPERADORES COMPARACION NOCTURNA");
+
+console.log("Igualdad debil ==", "5"==5);
+console.log("Igualdad Estricta ===", "5"===5);
+console.log("Desigualdad debil !=", "5"!=5);
+console.log("Desigualdad Estricta !==", "5"!==5);
+console.log("Mayor que >", 5>3);
+console.log("Menor que <", 3<5);
+console.log("Mayor o Igual >=", 5>=5);
+console.log("Menor o Igual <=", 3<=5);
+
+console.log("OPERADORES LOGICOS NOCTURNA");
+console.log("AND && - El Ae86 fue el mas vendido, sigue disponible?", "5"<=5 && "5">=5);
+console.log("OR || - El Ae86 fue el mas vendido o el GR86 es mas caro?", "5"<=5 || "6">=4);
+console.log("NOT ! - El Ae86 no fue el mas vendido?", !(5==5));
+
+
+
+
+
