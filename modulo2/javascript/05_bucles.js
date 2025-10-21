585a6d19548a35225401df852e875d4e259f5e82
natcore-tech <asesoriaelectr@gmail.com>
2025-10-21 10:02:53 -0500

    Clase 2025-10-21

---
+++ modulo2/javascript/05_bucles.js
@@ -0,0 +1,18 @@
+for (let i=1; i <=10; i++) {
+    console.log("Numero",i);
+}
+suma=0;
+
+for (let i=1; i <=10; i++) {
+    suma+=i;
+}
+console.log("Sumatoria Indices", suma);
+
+for (let i=4; i <=40; i+=4) {
+    console.log("Table del 4",i);
+}
+
+nombre="Francisco";
+for (let i=0; i < nombre.length; i++) {
+    console.log(nombre[i]);
+}
