30452a0ed30c11bed5d99e13a7c90a26b73d3634
natcore-tech <asesoriaelectr@gmail.com>
2026-01-15 15:26:18 -0500

    Clase 2026-01-15

--- modulo3/nestjs/blog-backend/src/main.ts
+++ modulo3/nestjs/blog-backend/src/main.ts
@@ -1,19 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { GlobalHttpExceptionFilter } from './common/filters/http-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
-import { join } from 'path/win32';
+import { GlobalHttpExceptionFilter } from './common/filters/http-exception.filter';
+import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
+
+  // ✅ CORS (ajusta origins)
+  app.enableCors({
+    origin: [
+      'http://localhost:5173', // Vite
+      'http://localhost:3000', // si tu front usa este
+      'https://post-api.nael.live',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true, // si usas cookies/sesión
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
   app.useGlobalFilters(new GlobalHttpExceptionFilter());
-  app.useStaticAssets(join(__dirname, '..', 'public'),
-  {
-    prefix: '/public/',
-  }
-);
-  await app.listen( 3000);
+
+  app.useStaticAssets(join(__dirname, '..', 'public'), { prefix: '/public' });
+
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
